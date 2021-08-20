cb994ae25553a86ce4beb3462ef505e88f53af34
BorjaG90 <borjag90dev@gmail.com>
2021-08-20 13:58:53 +0200

    feat: add Product component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  {
+    path: 'products', // lazy loading
+    loadChildren: () =>
+      import('./pages/products/products.module').then((m) => m.ProductsModule),
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
 @NgModule({
