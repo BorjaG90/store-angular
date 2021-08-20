0e3be833932185ea33c092ef38d9af6bbbecfdfd
BorjaG90 <borjag90dev@gmail.com>
2021-08-20 14:10:31 +0200

    feat: first http request

--- src/app/pages/products/services/products.service.ts
+++ src/app/pages/products/services/products.service.ts
@@ -1,15 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Product } from '../interfaces/product.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  private apiURL = '';
+  private apiURL = 'http://localhost:3000/products';
   constructor(private http: HttpClient) { }
 
-  getProducts(): Observable<any> {
-    return this.http.get(this.apiURL);
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiURL);
   }
 }
