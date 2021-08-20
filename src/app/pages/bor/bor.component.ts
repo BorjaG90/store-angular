14cca29a3e9ffd4cb5066d284181f010bb8c5db2
BorjaG90 <borjag90dev@gmail.com>
2021-08-20 13:47:27 +0200

    feat: add bor component

---
+++ src/app/pages/bor/bor.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bor',
+  templateUrl: './bor.component.html',
+  styleUrls: ['./bor.component.html'],
+})
+export class BorComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
