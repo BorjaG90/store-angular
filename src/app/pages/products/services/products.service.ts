41332ebbd25f5baad29086eddb8f01305956e710
BorjaG90 <borjag90dev@gmail.com>
2021-08-20 14:08:40 +0200

    feat: httpClientModule

--- src/app/pages/products/services/products.service.ts
+++ src/app/pages/products/services/products.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  
-  constructor() { }
+  private apiURL = '';
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<any> {
+    return this.http.get(this.apiURL);
+  }
 }
