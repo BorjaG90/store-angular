05f1cdfc8b7b5c4910bbb58bd7cbc798771e2c39
BorjaG90 <borjag90dev@gmail.com>
2021-08-24 12:48:58 +0200

    feat: render stores

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { Store } from 'src/app/shared/intefaces/store.interface';
+import { DataService } from 'src/app/shared/services/data.service';
 
 @Component({
   selector: 'app-checkout',
@@ -12,33 +15,28 @@ export class CheckoutComponent implements OnInit {
     shippingAddress: '',
     city: '',
   };
-  stores= [
-    {
-      "id": 1,
-      "name": "Park Row at Beekman St",
-      "address": "38 Park Row",
-      "city": "New York",
-      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
-    },
-    {
-      "id": 2,
-      "name": "Store Alcalá",
-      "address": "Calle de Alcalá, 21",
-      "city": "Madrid",
-      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
-    },
-  ]
+  stores: Store[] = [
+    
+  ];
 
-  constructor() {}
+  constructor(private dataSvc: DataService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getStores();
+  }
 
   onPickupOrDelivery(value: boolean): void {
     console.log(value);
   }
 
   onSubmit(): void {
-    console.log("Guardar");
-    
+    console.log('Guardar');
+  }
+
+  getStores(): void {
+    this.dataSvc
+      .getStores()
+      .pipe(tap((stores: Store[]) => this.stores = stores))
+      .subscribe();
   }
 }
