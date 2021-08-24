7025a6703f2fd5d5c54edafa25cb7e5d58aff41a
BorjaG90 <borjag90dev@gmail.com>
2021-08-24 13:17:33 +0200

    feat: save from

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,20 +1,24 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { Store } from "../intefaces/store.interface";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '../intefaces/store.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class DataService {
   private apiUrl = 'http://localhost:3000';
-  constructor(private http: HttpClient) {
-    
+  constructor(private http: HttpClient) {}
+
+  getStores(): Observable<Store[]> {
+    return this.http.get<Store[]>(`${this.apiUrl}/stores`);
   }
 
-  getStores(): Observable<Store[]>{
-    return this.http.get<Store[]>(`${this.apiUrl}/stores`)
+  saveOrders(order: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/orders`, order);
   }
-  
+
+  saveDetailsOrder(details: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/detailsOrders`, details);
+  }
 }
