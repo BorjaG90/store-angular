49382c2351907593c3fe2e1c380b65b713df1ba1
BorjaG90 <borjag90dev@gmail.com>
2021-08-25 09:43:20 +0200

    refactor: update & shrink project

--- src/app/pages/checkout/thank-you-page/thank-you-page.component.ts
+++ src/app/pages/checkout/thank-you-page/thank-you-page.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-thank-you-page',
-  templateUrl: './thank-you-page.component.html',
-  styleUrls: ['./thank-you-page.component.scss']
+  template: `<div class="container">
+    <h1 class="title">Thank you!</h1>
+    <p class="content">Your order is on the way</p>
+    <span
+      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corporis
+      perspiciatis distinctio voluptatibus. Eos iure ad in porro atque ex
+      pariatur sed quisquam, tenetur, ab illum vel facere est
+      perspiciatis.</span
+    >
+  </div>`,
+  styleUrls: ['./thank-you-page.component.scss'],
 })
-export class ThankYouPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ThankYouPageComponent {}
