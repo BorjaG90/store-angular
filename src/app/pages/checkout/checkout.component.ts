1533001541fb15b1add34aa1cd98b5a259d5472e
BorjaG90 <borjag90dev@gmail.com>
2021-08-25 09:22:26 +0200

    feat: add resetCart

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -45,7 +45,7 @@ export class CheckoutComponent implements OnInit {
     const data: Order = {
       ...formData,
       date: this.getCurrentDate(),
-      pickup: this.isDelivery,
+      isDelivery: this.isDelivery,
     };
 
     this.dataSvc
